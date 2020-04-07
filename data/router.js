dbf4d13a997613c7e395664aff230a6249e7482e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-07 14:54:12 -0600

    git request for pulling individual posts now works

--- data/router.js
+++ data/router.js
@@ -1,7 +1,7 @@
 // Needed 
 // Post /api/posts, 
 // Post /api/posts/:id/comments,
-// Get /api/posts,
+// SOLVED!!!! Get /api/posts,
 // Get /api/posts/:id,
 // Get /api/posts/:id/comments,
 // DELETE /api/posts/:id,
@@ -27,6 +27,21 @@ router.get("/", (req, res)=> {
     }
     
     )
+})
+
+router.get("/:id", (req, res)=> {
+    dB.findById(req.params.id)
+    .then((post)=>{
+        if(post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    })
+    .catch((error)=>{
+        console.log(error)
+        res.status(500).json({error: "The post information could not be retrieved."})
+    })
 })
 
 module.exports = router;
